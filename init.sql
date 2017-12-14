1940da6ca08eccb8be36ccd374017ba8029fb2a5
Nic Ferrier <nic@ferrier.me.uk>
2017-12-14 04:37:56 +0000

    init the db via a sql script

---
+++ init.sql
@@ -0,0 +1,69 @@
+-- -*- mode: sql-mode -*-
+
+CREATE OR REPLACE FUNCTION schema_init () RETURNS void AS $$
+begin
+    SET client_min_messages = error;
+
+    -- Routes
+    CREATE SEQUENCE IF NOT EXISTS route_ids;
+    
+    CREATE TABLE IF NOT EXISTS route ("id" INTEGER,
+                                      "path" TEXT,
+                                      "port" INTEGER);
+
+    PERFORM id FROM route WHERE path = '/';
+    if not found then
+        INSERT INTO route (id, path, port)
+        VALUES (nextval('route_ids'), '/', 8001);
+    end if;
+
+    -- Wiki
+    CREATE SEQUENCE IF NOT EXISTS wiki_ids;
+    
+    CREATE TABLE IF NOT EXISTS wiki ("id" INTEGER,
+                                     "date" TIMESTAMP WITH TIME ZONE,
+                                     "author" TEXT,
+                                     "name" TEXT,
+                                     "content" JSON);
+
+    PERFORM id FROM wiki WHERE name = 'Main' ORDER BY id DESC LIMIT 1;
+    if not found then
+        INSERT INTO wiki (id,
+                          author,
+                          name,
+                          content,
+                          date)
+        VALUES (nextval('wiki_ids'),
+                'Crudi',
+                'Main',
+                '[{"h1": "Main Page"}]'::json,
+                now());
+    end if;
+
+    -- Tickets
+    CREATE SEQUENCE IF NOT EXISTS ticket_ids;
+    
+    CREATE TABLE IF NOT EXISTS ticket (id INTEGER,
+                                       date TIMESTAMP WITH TIME ZONE,
+                                       title TEXT,
+                                       author TEXT,
+                                       assigned TEXT,
+                                       description JSON, 
+                                       comments JSON);
+
+    PERFORM id FROM ticket WHERE title = 'First';
+    if not found then
+        INSERT INTO ticket (id, date, title, 
+                            author, assigned, 
+                            description, comments) 
+        VALUES (nextval('ticket_ids'),
+                now(),
+                'an example ticket',
+                'nicferrier', 'nicferrier',
+                '{}', '{}');
+    end if;
+
+END;
+$$ LANGUAGE plpgsql;
+  
+-- init.sql ends here
--- src/crudi/crudidb.cr
+++ src/crudi/crudidb.cr
@@ -1,6 +1,8 @@
 require "db"
 require "pg"
 require "json"
+require "file"
+require "io"
 
 # DB routines
 module CrudiDb
@@ -57,81 +59,14 @@ VALUES (nextval('wiki_ids'), 'Crudi', $1, $2, now())",
   end
 
   def self.initdb
+    file = File.open("init.sql")
+    doc = IO::Memory.new
+    IO.copy file, doc
+    
     DB.open "postgres://crudi@localhost/crudi" do |db|
       # Create tables and stuff
-      db.exec "CREATE SEQUENCE IF NOT EXISTS route_ids"
-
-      ## db.exec "DROP TABLE IF EXISTS route"
-      route_fields = ["id INTEGER",
-                      "path TEXT",
-                      "port INTEGER"]
-      route_field_spec = route_fields.join(",")
-      db.exec "CREATE TABLE IF NOT EXISTS route (#{route_field_spec})"
-
-      # And some beginning data
-      res = db.query "SELECT id FROM route WHERE path = '/'"
-      if ! res.move_next
-        db.exec "INSERT INTO route (id, path, port)
- VALUES (nextval('route_ids'), $1, $2)", "/", 8001
-      end
-
-      # Tables for wiki
-      ## db.exec "DROP TABLE IF EXISTS wiki"
-      
-      db.exec "CREATE SEQUENCE IF NOT EXISTS wiki_ids"
-      wiki_fields = ["id INTEGER",
-                     "date TIMESTAMP WITH TIME ZONE",
-                     "author TEXT",
-                     "name TEXT", # the page name?
-                     "content JSON"]
-      wiki_field_spec = wiki_fields.join(",")
-      db.exec "CREATE TABLE IF NOT EXISTS wiki (#{wiki_field_spec})"
-
-      # delete the Main page and recreate
-      ## db.exec "DELETE FROM wiki WHERE name='Main'"
-
-      begin
-        id = db.query_one "SELECT id 
-FROM wiki 
-WHERE name = 'Main'
-ORDER BY id DESC
-LIMIT 1", &.read(Int)
-      rescue ex : DB::Error
-        puts "CrudiDb.initdb error for wiki table #{ex.message}"
-        json = %q([{"h1": "Main page"}])
-        db.exec "INSERT INTO wiki (id, author, name, content, date) 
-VALUES (nextval('wiki_ids'), 'Crudi', 'Main', $1, now())", json
-      end
-
-      # Tables for tickets
-      ## db.exec "DROP TABLE IF EXISTS ticket"
-      
-      db.exec "CREATE SEQUENCE IF NOT EXISTS ticket_ids"
-      ticket_fields = ["id INTEGER",
-                       "date TIMESTAMP WITH TIME ZONE",
-                       "title TEXT",
-                       "author TEXT",
-                       "assigned TEXT",
-                       "description JSON", # probably conforming to a doc schema we'll make
-                       "comments JSON"]
-      ticket_field_spec = ticket_fields.join(",")
-      db.exec "CREATE TABLE IF NOT EXISTS ticket (#{ticket_field_spec})"
-
-      # delete the Main page and recreate
-      ## db.exec "DELETE FROM wiki WHERE name='Main'"
-
-      begin
-        id = db.query_one "SELECT id FROM ticket WHERE title = 'First'", &.read(Int)
-      rescue ex : DB::Error
-        json = %q([{"h1": ""}])
-        db.exec "INSERT INTO ticket (id, date, title, 
-author, assigned, 
-description, comments) 
-VALUES (nextval('ticket_ids'), now(), 'an example ticket',
-$1, $1,
-'{}', '{}')", "nicferrier"
-      end
-
+      db.exec doc.to_s
+      db.exec "select schema_init();"
     end
   end
 end
